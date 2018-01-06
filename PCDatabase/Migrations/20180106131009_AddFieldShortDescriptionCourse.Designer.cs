303862453083d7e3d56d465f6f762ab670b49305
Micał Jakub <mical.jakub@gmail.com>
2018-01-06 14:12:38 +0100

    Added field ShortDescriptionCourse to database

--- PCDatabase/Migrations/20180106131009_AddFieldShortDescriptionCourse.Designer.cs
+++ PCDatabase/Migrations/20180106131009_AddFieldShortDescriptionCourse.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace PCDatabase.Migrations
 {
     [DbContext(typeof(PCContext))]
-    partial class PCContextModelSnapshot : ModelSnapshot
+    [Migration("20180106131009_AddFieldShortDescriptionCourse")]
+    partial class AddFieldShortDescriptionCourse
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PCDatabase/Migrations/20180106131009_AddFieldShortDescriptionCourse.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace PCDatabase.Migrations
+{
+    public partial class AddFieldShortDescriptionCourse : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ShortDescriptionCourse",
+                table: "Courses",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShortDescriptionCourse",
+                table: "Courses");
+        }
+    }
+}
